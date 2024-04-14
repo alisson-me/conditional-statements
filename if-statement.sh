5fcab3658541f49c54cabec2f3250d4393535ecf
alisson-me <alison-me@outlook.com>
2024-04-14 10:50:41 +0000

    if-statement syntax and example.

---
+++ if-statement.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+        # if-statement syntax
+
+# if [ condition ]; then
+
+# condition to execute when true.
+
+# fi
+
+        # Example
+
+	# Set variables, if required.
+
+	# Ask for user's age.
+echo "How old are you?"
+read age
+
+if [ $age -ge 18 ]; then
+
+echo "The condition is true."
+
+fi
+
+
+	#Good work!
